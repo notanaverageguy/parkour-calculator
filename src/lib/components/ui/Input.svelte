d077184cf342d01eae1bbe7079fac7b47239860e
notanaverageguy <83960401+notanaverageguy@users.noreply.github.com>
2025-11-27 15:21:55 -0500

    Made inputs handle commas and made them also format nicely when typing

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -1,9 +1,30 @@
-<script>
+<script lang="ts">
 	export let label = '';
 	export let value = 0;
+
+	let displayValue = value.toLocaleString();
+
+	function handleInput(e: Event) {
+		const target = e.target as HTMLInputElement;
+
+		const raw = target.value.replace(/,/g, '');
+
+		const num = Number(raw);
+		if (!Number.isNaN(num)) {
+			value = num;
+			displayValue = target.value;
+		}
+	}
+
+	$: displayValue = value.toLocaleString();
 </script>
 
 <label>
 	<p class="mb-2">{label}</p>
-	<input type="number" bind:value class="w-full rounded-md border-none bg-gray-800 py-2 text-lg" />
+	<input
+		type="text"
+		class="w-full rounded-md border-none bg-gray-800 py-2 text-lg"
+		bind:value={displayValue}
+		on:input={handleInput}
+	/>
 </label>
