b71ae2b1c719d8d587e07e067fd262ad229a5af5
notanaverageguy <83960401+notanaverageguy@users.noreply.github.com>
2025-11-27 02:56:09 -0500

    idk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	export const prerender = true;
+
+	export const trailingSlash = 'always';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+
 	let { children } = $props();
 </script>
 
+export const prerender = true; export const trailingSlash = "always";
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
